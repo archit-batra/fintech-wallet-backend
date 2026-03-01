4daede32c948a968858766168946be2753bc677e
Archit Batra <dev@architbatra.in>
2026-03-01 09:24:35 +0530

    feat: implement redis-based worker for async transfer processing

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -2,21 +2,28 @@ package main
 
 import (
 	"log"
-	"time"
 
-	"github.com/archit-batra/fintech-wallet-backend/internal/events"
+	"github.com/archit-batra/fintech-wallet-backend/internal/infra"
 )
 
 func main() {
 
 	log.Println("Worker started...")
 
+	redisClient := infra.NewRedisClient()
+
 	for {
-		select {
-		case event := <-events.EventQueue:
-			log.Printf("Processing event: %s | %s", event.Type, event.Data)
-		default:
-			time.Sleep(1 * time.Second)
+		result, err := redisClient.BRPop(
+			infra.Ctx,
+			0,
+			"transfer_queue",
+		).Result()
+
+		if err != nil {
+			log.Println("Error receiving job:", err)
+			continue
 		}
+
+		log.Println("Processing job:", result[1])
 	}
 }
